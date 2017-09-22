e6bff6b98ae5aa5c7dce3bea22f7e2e558e6be4f
lindwurm <lindwurm.q@gmail.com>
2017-09-22 22:53:41 +0900

    Revert "Change IDs to strings rather than numbers in API JSON output (#5019)"
    This reverts commit 669fe9ee06a82482201377abd303492eb7fa7d94.
    
    temp
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- app/serializers/rest/application_serializer.rb
+++ app/serializers/rest/application_serializer.rb
@@ -4,12 +4,8 @@ class REST::ApplicationSerializer < ActiveModel::Serializer
   attributes :id, :name, :website, :redirect_uri,
              :client_id, :client_secret
 
-  def id
-    object.id.to_s
-  end
-
   def client_id
-    object.uid.to_s
+    object.uid
   end
 
   def client_secret
